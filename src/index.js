4b589181047b9e4a4cab43acb398a8a8f40dc7a5
Iryna Hirnyk <iryna.gir@gmail.com>
2020-10-01 21:45:33 +0300

    init: start typical-arrays-problems-task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
+exports.min = function min(array) {
+    let minNumber = 0;
+    if (array === undefined || arguments.length === 0 || array.length === 0) {
+        minNumber = 0;
+        return minNumber;
+    }
+    return Math.min(...array);
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    let maxNumber = 0;
+    if (array === undefined || arguments.length === 0 || array.length === 0) {
+        maxNumber = 0;
+        return maxNumber;
+    }
 
-exports.max = function max (array) {
-  return 0;
-}
+    return Math.max(...array);
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    let avgNumber = 0;
+    if (array === undefined || arguments.length === 0 || array.length === 0) {
+        avgNumber = 0;
+        return avgNumber;
+    }
+    let result = array.reduce(function (a, b) {
+        return a + b;
+    }, 0);
+    return result / array.length;
+};
